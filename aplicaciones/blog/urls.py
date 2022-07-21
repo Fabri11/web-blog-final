baf15881157724db69e830196dc5ce847678bbcf
Fabrizio <fabrisabella25@gmail.com>
2022-07-21 10:33:34 -0300

    fix nav, post, detail post, etc

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import home, post, proyectos
+from .views import *
 
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('detalles_post/<slug:slug>/', detallesPost, name = 'detalles_post'),
     path('proyectos', proyectos, name = 'proyectos'),
     path('post', post, name = 'post'),
+    path('videojuegos', videojuegos, name = 'videojuegos')
 ]
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,28 +1,81 @@
-from django.shortcuts import render
-from .models import Project, Post
 
+from .models import *
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
+    posts = Post.objects.all().order_by('-fecha_creacion')[:]
+
+    return render(request, 'home.html',{'posts':posts})
+
+
+def error404(request):
+    return render(request, 'not_found.html')
+
+# ------------------------Search Navbar-----------------------------------------
+
+def search(request):
     queryset = request.GET.get("buscar")
-    print(queryset)
-    projects = Project.objects.all()
+    posts = Post.objects.filter(estado = True)
     if queryset:
-        projects = Project.objects.filter(
+        posts = Project.objects.filter(
             Q(titulo__icontains = queryset) |
             Q(descripcion__icontains = queryset)
         ).distinct()
 
-    return render(request, 'home.html',{'projects':projects})
+    print(posts)
 
+    return render(request, '_navbar.html')
+
+# --------------------------Paginator pages-------------------------------------
+    
+
+#------------------------Post y detalles----------------------------------------
+def post(request):
+    posts = Post.objects.all()
+
+    posts = Post.objects.all().order_by('-fecha_creacion')[:2]
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request, 'post.html', {'posts': posts})
+
+def detallesPost(request, slug):
+    posts = Post.objects.filter(slug = slug)
+    print(posts)
+    return render(request, 'detalles_post.html', {'posts':posts})
+
+# ------------------------- Pages----------------------------------------------------
 
 def proyectos(request):
-    project = Project.objects.all()
-    return render(request, 'proyectos.html', {'project':project})
-
-def post(request):
-    post = Post.objects.all()
-    return render(request, 'post.html', {'post': post})
+    projects = Project.objects.all()
+    return render(request, 'proyectos.html', {'projects':projects})
 
 
+#------------------- Vistas de las diferentes categorias-------------------------------
+
+def tecnologia(request):
+    posts = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.filter(nombre__iexact = 'tecnologia')
+    )
+    return render(request, 'tecnologia.html', {'posts':posts})
+
+def general(request):
+    posts = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.get(nombre__iexact  = 'General')
+    )
+    return render(request, 'general.html', {'posts':posts})
+
+def videojuegos(request):
+    posts = Post.objects.filter(
+        estado = False,
+        categoria = Categoria.objects.get(nombre__iexact  = 'VideoJuegos')
+    )
+    return render(request, 'categorias/videojuegos.html', {'posts':posts})
 
